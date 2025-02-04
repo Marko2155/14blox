7f47444f39538d100680dd6423358145720bb7b3
Marko2155 <welsheyjack@gmail.com>
2025-02-04 19:55:00 +0100

    hi

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require("http")
 const url = require("url")
 const fs = require("fs")
+const { wrap } = require("module")
 const host = "0.0.0.0"
-const port = 8080
+const port = 10000
 
 function SendFile(res, file) {
     let filetext = fs.readFileSync(file);
@@ -13,22 +14,33 @@ function WriteNewline(res, text) {
     res.write(text + "\n")
 }
 
+function JSONToArray(json){
+    var result = [];
+    var keys = Object.keys(json);
+    keys.forEach(function(key){
+        result.push(json[key]);
+    });
+    return result;
+}
+
+function GetUserData(username, db) {
+    let userdb = JSON.parse(fs.readFileSync("db/" + db + "/users.json"))
+    return userdb.find(user => user.UserName == username)
+}
 http.createServer(function(req, res) {
     const urlpath = url.parse(req.url, true)
     const path = urlpath.path
     if (req.method == "GET") {
+        console.log("GET " + path)
         if (path == "/") {
-            console.log("GET " + path)
             res.writeHead(200);
             SendFile(res, "index.html")
             res.end();
         } else if (path == "/games/list") {
-            console.log("GET " + path)
             res.writeHead(200);
             SendFile(res, "games/list.html")
             res.end();
         } else if (path == "/games/123456789") {
-            console.log("GET " + path)
             res.writeHead(200);
             SendFile(res, "games/123456789.rbxl")
             res.end();
@@ -38,7 +50,41 @@ http.createServer(function(req, res) {
             res.end();
         }
     } else if (req.method == "POST") {
+        console.log("POST " + path)
+        var body = "";
+        req.on("data", function(chunk) {
+            body += chunk;
+        })
 
+        req.on("end", function() {
+            if (path == "/mobileapi/login") {
+                console.log(body);
+                res.writeHead(200);
+                const userdata = body.split("&");
+                const password = userdata[1].split("=")[1];
+                const username = userdata[0].split("=")[1];
+                const userData = GetUserData(username, "devl")
+                let finishedData = {
+                    Status: "",
+                    UserInfo: ""
+                }
+                console.log(userData);
+                if (userData != null && userData.UserPassword != null && userData.UserPassword == password) {
+                    finishedData.Status = "OK"
+                    finishedData.UserInfo = userData;
+                    res.write(JSON.stringify(finishedData))
+                    res.end();
+                } else {
+                    finishedData.Status = "InvalidPassword"
+                    finishedData.UserInfo = "";
+                    res.write(JSON.stringify(finishedData));
+                    res.end();
+                }
+            } else {
+                res.write("what are you doing here, this API call doesn't exist.")
+                res.writeHead(404);
+            }
+        })
     }
 }).listen(port, host, function() {
     console.log(`Listening at port ${port}`)
