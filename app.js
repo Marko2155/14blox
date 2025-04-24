5e910d3b06d3de3efce44d920460c64ca36cfc9e
Marko2155 <welsheyjack@gmail.com>
2025-04-24 19:12:21 +0200

    somehow sessions got f'd

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const host = "0.0.0.0"
 const port = 10000
 let key = "1122334455667788998877665544332211"
 let DMPerror = "";
+let sessions = []
 
 const client = new MongoClient(uri, {
   serverApi: {
