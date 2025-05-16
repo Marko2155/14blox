920af2d4c7da77f19a6c52520a654a1afceb3794
Marko2155 <welsheyjack@gmail.com>
2025-05-16 10:21:00 +0200

    broke something then fixefd

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ http.createServer(async function(req, res) {
 						BodyColors: bodyColors
 					}
 				}
-				await client.db("14blox").collection("avatars").updateOne({ uid: sessions[req.connection.remoteAddress.replaceAll(".", "")].UserID }, updateDocument})
+				await client.db("14blox").collection("avatars").updateOne({ uid: sessions[req.connection.remoteAddress.replaceAll(".", "")].UserID }, updateDocument)
 				res.end()
 			}
 
